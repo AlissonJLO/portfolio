4c581aa60ec3ff8f015f6fbd0b61628d2b75c73b
Alisson Joaquim lara oliveira <alissonloaquim@hotmail.com>
2024-04-18 13:23:21 -0400

    add:responsividade

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt" className="white">
+    <html lang="pt-br" className="white">
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import Perfil from "../sections/Perfil";
 import Projetos from "../sections/Projetos";
 import Footer from "../sections/Footer";
 
-export default function Home() {
+export default function Home(): React.JSX.Element {
   return (
-    <main className="flex space-x-10 ">
-      <Perfil />
-      <Projetos />
-      <Footer />
-    </main>
+    <>
+      <main className="md:flex-row md:space-x-10 flex flex-col flex-1 min-h-dvh min-w-dvw">
+        <Perfil />
+        <Projetos />
+      </main>
+      <footer className="fixed inset-x-0 bottom-0 bg-white text-black p-4">
+        <Footer />
+      </footer>
+    </>
   );
 }

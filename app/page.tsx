ea9c294772dbaf1bca5393b02b1a5cf93c7cdf3f
Alisson Joaquim lara oliveira <alissonloaquim@hotmail.com>
2024-04-18 01:45:34 -0400

    add:sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@nextui-org/button";
+import Perfil from "../sections/Perfil";
+import Projetos from "../sections/Projetos";
+import Footer from "../sections/Footer";
 
 export default function Home() {
   return (
-  
-      <Button>Button</Button>
-
+    <main className="flex space-x-10 ">
+      <Perfil />
+      <Projetos />
+      <Footer />
+    </main>
   );
 }
